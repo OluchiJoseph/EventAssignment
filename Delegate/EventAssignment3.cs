a5c1be001a45ec81cb9672ccf182ba588340cb32
support <support@WEMA-TRAINING-S>
2022-08-16 10:40:44 -0700

    Add project files.

---
+++ Delegate/EventAssignment3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    internal class StatementAnalysis  //publisher
+    {
+        public delegate void AlertHandler();
+        public event AlertHandler AlertTheClasses;
+
+        public void ForgeryDetection()
+        {
+            //codes codes
+            Console.WriteLine("Forgery detection in progress");
+            System.Threading.Thread.Sleep(10000);
+            //when forgery is detected
+            OnForgeryDetected();
+        }
+        protected virtual void OnForgeryDetected()
+        {
+            AlertTheClasses?.Invoke();  //raise event
+        }
+
+    }
+
+    public class Inspector   //subscriber
+    {
+        public void ActOnImposter()
+        {
+            var stateAn = new StatementAnalysis();
+            stateAn.AlertTheClasses += ForgeryDetectionActions;
+            stateAn.AlertTheClasses += () => Console.WriteLine("Alert police to catch suspects");
+
+            stateAn.ForgeryDetection();
+        }
+
+
+
+        public void ForgeryDetectionActions()
+        {
+            Console.WriteLine("Alert police to catch suspects");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    internal class StatementAnalysis  //publisher
+    {
+        public delegate void AlertHandler();
+        public event AlertHandler AlertOtherClasses;
+
+        public void ImposterDetection()
+        {
+            //codes codes
+            Console.WriteLine("Imposter detection in progress");
+            System.Threading.Thread.Sleep(10000);
+            //when imposter is detected
+            OnImposterDetected();
+        }
+        protected virtual void OnImposterDetected()
+        {
+            AlertOtherClasses?.Invoke();  //raise event
+        }
+
+    }
+
+    public class Inspector   //subscriber
+    {
+        public void ActOnImposter()
+        {
+            var stateAn = new StatementAnalysis();
+            stateAn.AlertOtherClasses += ImposterDetectionActions;
+            stateAn.AlertOtherClasses += () => Console.WriteLine("Alert police to catch suspects");
+
+            stateAn.ImposterDetection();
+        }
+
+
+
+        public void ImposterDetectionActions()
+        {
+            Console.WriteLine("Alert police to catch suspects");
+            Console.ReadLine();
+        }
+    }
+}
